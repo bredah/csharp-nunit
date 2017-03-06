fd5f80d51bec9db1f4551e720ba2260e8b23f737
henbreda <henbreda@SAO-6HC8CD2>
2017-03-05 23:28:01 -0300

    Add project files.

---
+++ CSharp/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Calculator
+    {
+        public enum Operations
+        {
+            Add,
+            Sub,
+            Mult,
+            Div,
+        }
+
+        public int valueA { get; set; }
+        public int valueB { get; set; }
+
+        public int getResult(Operations calc)
+        {
+            int result = 0;
+            switch (calc)
+            {
+                case Operations.Add:
+                    result = valueA + valueB;
+                    break;
+                case Operations.Sub:
+                    result = valueA - valueB;
+                    break;
+                case Operations.Mult:
+                    result = valueA * valueB;
+                    break;
+                case Operations.Div:
+                    result = valueA / valueB;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ CSharpTests/CalculatorTest.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator calculator;
+        private int valueA = 0;
+        private int valueB = 0;
+
+        [SetUp]
+        protected void setUp()
+        {
+            calculator = new Calculator();
+            valueA = 4;
+            valueB = 2;
+        }
+
+        [TearDown]
+        protected void tearDown()
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            Assert.AreEqual(valueA + valueB, calculator.getResult(Calculator.Operations.Add));
+        }
+
+        [Test]
+        public void testGetResultAdd()
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            Assert.AreEqual(valueA + valueB, calculator.getResult(Calculator.Operations.Add));
+        }
+
+        [Test, Combinatorial]
+        public void testGetResultAddCombinatorial([Values(2, 4, 6)] int a, [Values(1, 3)] int b)
+        {
+            calculator.valueA = a;
+            calculator.valueB = b;
+            Assert.AreEqual(a + b, calculator.getResult(Calculator.Operations.Add));
+        }
+
+        [Test, Sequential]
+        public void testGetResultAddSequential([Values(2, 4, 6)] int a, [Values(1, 3)] int b)
+        {
+            calculator.valueA = a;
+            calculator.valueB = b;
+            Assert.AreEqual(a + b, calculator.getResult(Calculator.Operations.Add));
+        }
+
+        [Test]
+        public void testGetResultSub()
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            Assert.AreEqual(valueA - valueB, calculator.getResult(Calculator.Operations.Sub));
+        }
+
+        [Test]
+        public void testGetResultMult()
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            Assert.AreEqual(valueA * valueB, calculator.getResult(Calculator.Operations.Mult));
+        }
+
+        [Test]
+        public void testGetResultDiv()
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            Assert.AreEqual(valueA / valueB, calculator.getResult(Calculator.Operations.Div));
+        }
+
+        [TestCaseSource("GetOperations")]
+        public void testGetResulOperation(Calculator.Operations operation)
+        {
+            calculator.valueA = valueA;
+            calculator.valueB = valueB;
+            calculator.getResult(operation);
+        }
+
+        /// <summary>
+        /// Retrieve all value from enum 
+        /// </summary>
+        /// <returns>Array with enum values</returns>
+        private static Array GetOperations()
+        {
+            return Enum.GetValues(typeof(Calculator.Operations));
+        }
+    }
+}
